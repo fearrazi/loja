5593c45c8e158f2f65fab5495a54af082a21dc56
FERNANDA SILVA/LGESP CNS - SSP(arrazi.silva@lgepartner.com) <arrazi.silva@lge.com>
2021-03-22 11:28:00 -0300

    atualizando versão da alura

--- src/main/java/br/com/alura/microservice/loja/dto/InfoFornecedorDTO.java
+++ src/main/java/br/com/alura/microservice/loja/dto/InfoFornecedorDTO.java
@@ -1,14 +1,14 @@
 package br.com.alura.microservice.loja.dto;
 
 public class InfoFornecedorDTO {
-	
-		private String endereco;
 
-		public String getEndereco() {
-			return endereco;
-		}
+	private String endereco;
 
-		public void setEndereco(String endereco) {
-			this.endereco = endereco;
-		}
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
 }
--- src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,25 +1,89 @@
 package br.com.alura.microservice.loja.service;
 
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import java.time.LocalDate;
 
-import br.com.alura.microservice.loja.controller.dto.CompraDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+import br.com.alura.microservice.loja.client.FornecedorClient;
+import br.com.alura.microservice.loja.client.TransportadorClient;
+import br.com.alura.microservice.loja.dto.CompraDTO;
+import br.com.alura.microservice.loja.dto.InfoEntregaDTO;
 import br.com.alura.microservice.loja.dto.InfoFornecedorDTO;
+import br.com.alura.microservice.loja.dto.InfoPedidoDTO;
+import br.com.alura.microservice.loja.dto.VoucherDTO;
+import br.com.alura.microservice.loja.model.Compra;
+import br.com.alura.microservice.loja.model.CompraState;
+import br.com.alura.microservice.loja.repository.CompraRepository;
 
 @Service
 public class CompraService {
-
-	public void realizaCompra(CompraDTO compra) {
-		RestTemplate client = new RestTemplate(); 
-		//ferramenta do Spring para requisições HTTP síncronas (get, post...) através de alguns métodos
-		//Vamos usar o método exchange
-		ResponseEntity<InfoFornecedorDTO> exchange = 
-		client.exchange("http://fornecedor:8081/info/"+compra.getEndereco().getEstado()
-				,HttpMethod.GET, null, InfoFornecedorDTO.class);
+	
+	private static final Logger LOG =  LoggerFactory.getLogger(CompraService.class);
+	
+	@Autowired
+	private FornecedorClient fornecedorClient;
+	
+	@Autowired
+	private TransportadorClient transportadorClient;
+	
+	@Autowired
+	private CompraRepository compraRepository;
+	
+	@HystrixCommand(threadPoolKey = "getByIdThreadPool")
+	public Compra getById(Long id) {
+		return compraRepository.findById(id).orElse(new Compra());
+	}
+	
+	public Compra reprocessaCompra(Long id) {
+		return null;
+	}
+	
+	public Compra cancelaCompra(Long id) {
+		return null;
+	}
+	
+	@HystrixCommand(fallbackMethod = "realizaCompraFallback",
+			threadPoolKey = "realizaCompraThreadPool")
+	public Compra realizaCompra(CompraDTO compra) {
+		Compra compraSalva = new Compra();
+		compraSalva.setState(CompraState.RECEBIDO);
+		compraSalva.setEnderecoDestino(compra.getEndereco().toString());
+		compraRepository.save(compraSalva);
+		compra.setCompraId(compraSalva.getId());
 		
-		System.out.println(exchange.getBody().getEndereco());
+		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
+		InfoPedidoDTO pedido = fornecedorClient.realizaPedido(compra.getItens());
+		compraSalva.setState(CompraState.PEDIDO_REALIZADO);
+		compraSalva.setPedidoId(pedido.getId());
+		compraSalva.setTempoDePreparo(pedido.getTempoDePreparo());
+		compraRepository.save(compraSalva);
+		
+		InfoEntregaDTO entregaDto = new InfoEntregaDTO();
+		entregaDto.setPedidoId(pedido.getId());
+		entregaDto.setDataParaEntrega(LocalDate.now().plusDays(pedido.getTempoDePreparo()));
+		entregaDto.setEnderecoOrigem(info.getEndereco());
+		VoucherDTO voucher = transportadorClient.reservaEntrega(entregaDto);
+		compraSalva.setState(CompraState.RESERVA_ENTREGA_REALIZADA);
+		compraSalva.setDataParaEntrega(voucher.getPrevisaoParaEntrega());
+		compraSalva.setVoucher(voucher.getNumero());
+		compraRepository.save(compraSalva);
+		
+		return compraSalva;
+	}
+
+	public Compra realizaCompraFallback(CompraDTO compra) {
+		if(compra.getCompraId() != null) {
+			return compraRepository.findById(compra.getCompraId()).get();
+		}
+		
+		Compra compraFallback = new Compra();
+		compraFallback.setEnderecoDestino(compra.getEndereco().toString());
+		return compraFallback;
 	}
 
 }
